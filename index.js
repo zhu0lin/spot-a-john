1783c56b4110a507d54d17ea1a1897c74d29f967
zhu0lin <zhuolinli91@gmail.com>
2025-03-22 21:48:52 -0400

    Finish Vercel setup, about.ejs

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 import express from "express";
 import bodyParser from "body-parser";
-
+import path from "path"; 
+import { fileURLToPath } from "url";
 const app = express();
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.set('views', path.join(__dirname, 'views'));
+
+app.set('view engine', 'ejs');
+
+app.use(express.static(path.join(__dirname, './public'))); 
+
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static("public"));
 
 app.get("/", (req, res) => {
     res.render("about.ejs");
@@ -23,5 +32,5 @@ app.get("/db", (req, res) => {
 });
 
 export default (req, res) => {
-    app(req, res);  // Use Express to handle the request
+    app(req, res);
 };
