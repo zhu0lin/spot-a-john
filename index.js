a428a2d88bc7fa4a328acf722fdd3216ccecae27
zhu0lin <zhuolinli91@gmail.com>
2025-03-22 21:21:57 -0400

    vercel deployment setup

--- index.js
+++ index.js
@@ -2,28 +2,26 @@ import express from "express";
 import bodyParser from "body-parser";
 
 const app = express();
-const port = 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-
-app.get("/", async (req, res) => {
+app.get("/", (req, res) => {
     res.render("about.ejs");
 });
 
-app.get("/about", async (req, res) => {
+app.get("/about", (req, res) => {
     res.render("about.ejs");
 });
 
-app.get("/api", async (req, res) => {
+app.get("/api", (req, res) => {
     res.render("API_johns.ejs");
 });
 
-app.get("/db", async (req, res) => {
+app.get("/db", (req, res) => {
     res.render("db_johns.ejs");
 });
 
-app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
-});
+export default (req, res) => {
+    app(req, res);  // Use Express to handle the request
+};
