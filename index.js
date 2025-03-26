65d1ef44c20ba41fefe9ceaaffc93bacc023f96f
zhu0lin <zhuolinli91@gmail.com>
2025-03-25 23:42:09 -0400

    Updated cron job

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ import bodyParser from "body-parser";
 import path from "path";
 import 'dotenv/config'
 import { fileURLToPath } from "url";
-import { createClient } from '@supabase/supabase-js'
 
 const app = express();
 
@@ -14,10 +13,6 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, './public')));
 app.use(bodyParser.urlencoded({ extended: true }));
-
-const public_anon_key = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVsdWF2ZHhzenlia2VrZG1sc21hIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDI2OTYwMDAsImV4cCI6MjA1ODI3MjAwMH0.hSFDSGNthbk2TNJ8evmSrzQKxvqoygl57BMwGTWGM2U'
-const supabaseKey = process.env[`SUPABASE_KEY`]
-const supabase = createClient('https://uluavdxszybkekdmlsma.supabase.co', public_anon_key);
 
 app.get("/", (req, res) => {
     res.render("about.ejs");
--- src/cron-job-update.js
+++ src/cron-job-update.js
@@ -1,5 +1,7 @@
 import cron from "node-cron";
 import axios from "axios";
+import supabase from "./supabase-client.js";
+
 
 const config = {
     headers: {
@@ -7,14 +9,15 @@ const config = {
     }
 };
 
-const task = cron.schedule('* * * * */1', async() => { //update database once every day by calling API
+const task = cron.schedule('* * * * */1', async () => { //update database once every day by calling API
     await updateDatabase();
 }, {
     scheduled: true,
     timezone: "America/New_York"
-});   
+});
 
-const updateDatabase = async() => {
+
+const updateDatabase = async () => {
     try {
         const response = await axios.get('https://data.cityofnewyork.us/resource/i7jb-7jku.json', config);
         const data = response.data;
@@ -22,18 +25,19 @@ const updateDatabase = async() => {
         const formattedData = data.map(entry => ({
             facility_name: entry.facility_name,
             location_type: entry.location_type,
-            coordinate: entry.coordinate,
+            coordinate: entry.coordinate, //fix this
             operator: entry.operator,
             status: entry.status,
             hours_of_operation: entry.hours_of_operation,
-            last_updated: new Date().toISOString() 
+            last_updated: new Date().toISOString()
         }));
-
-        const {error} = await supabase
-            .from('api_data')
-            .upsert(formattedData, { onConflict : 'facility_name'} )
+        
+        const { error } = await supabase
+            .from('api_data') 
+            .upsert(formattedData, { onConflict: ['id'] });
 
         if (error) throw error;
+        console.log(" Database successfully updated");
     }
     catch (error) {
         console.error("Error updating database:", error.message);
